82c91e691ea137e565022a8c0b42f201282ac3d3
Roman-22 <zaruba195@yandex.ru>
2024-08-21 19:08:18 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {"Roman": 1987, "Katerina": 1988}
+print(my_dict)
+my_dict["Olga"] = 2010
+print(my_dict)
+my_dict.update({"Ksenia":2014,"Maria":2021})
+print(my_dict)
+my_dict.pop("Maria")
+print(my_dict)
+
+my_set = {1,2,1,2,3,4,5,5,4,3,3,}
+print(my_set)
+print(my_set.add(6), my_set.add(7))
+print(my_set)
+print(my_set.discard(4))
+print(my_set)
